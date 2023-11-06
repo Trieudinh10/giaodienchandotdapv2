27102e433c9dfffbc5092deda1e1d9ff33c4c607
Dinh Le Quang Trieu <dinhlequangtrieu@CMC.THACO>
2023-11-06 09:05:21 +0700

    initial

--- index.js
+++ index.js
@@ -22,6 +22,54 @@ app.get('/table_2', function(req,res)
     res.render('table_2');
 })
 
+app.get('/data', function(req,res)
+{
+    res.render('data');
+})
+
+app.get('/setting', function(req,res)
+{
+    res.render('setting');
+})
 
 
 
+
+
+
+
+//////////////////////////////////////////////////////////////////////////*************************//////////////////////////////////////////////////////////////////////////
+
+// KHỞI TẠO KẾT NỐI PLC
+var nodes7 = require('nodes7');  
+var conn_plc = new nodes7; //PLC1
+// Tạo địa chỉ kết nối (slot = 2 nếu là 300/400, slot = 1 nếu là 1200/1500)
+conn_plc.initiateConnection({port: 102, host: '10.14.84.228', rack: 0, slot: 1}, PLC_connected);
+
+//Bảng tag trong Visual studio code
+const tag = require('./public/js/tag.js');
+const tags_list = tag.tags_list();
+
+// GỬI DỮ LIỆu TAG CHO PLC
+// Tag Name load
+var taglodash = require('lodash'); // Chuyển variable sang array
+var tag_Listarr = taglodash.keys(tags_list);
+// GỬI DỮ LIỆu TAG CHO PLC
+function PLC_connected(err) {
+    if (typeof(err) !== "undefined") {
+        console.log(err); // Hiển thị lỗi nếu không kết nối đƯỢc với PLC
+    }
+    conn_plc.setTranslationCB(function(tag) {return tags_list[tag];});  // Đưa giá trị đọc lên từ PLC và mảng
+    conn_plc.addItems(tag_Listarr);
+}
+
+// Đọc dữ liệu từ PLC và đưa vào array tags
+var arr_tag_value = []; // Tạo một mảng lưu giá trị tag đọc về
+var obj_tag_value = []; // Tạo một mảng lưu giá trị tag đọc về
+function valuesReady(anythingBad, values) {
+    if (anythingBad) { console.log("Lỗi khi đọc dữ liệu tag"); } // Cảnh báo lỗi
+    var lodash = require('lodash'); // Chuyển variable sang array
+    arr_tag_value = lodash.map(values, (item) => item);
+    console.log("Data S1", arr_tag_value); // Hiển thị giá trị để kiểm tra
+    obj_tag_value = values;
+}
---
+++ public/js/tag.js
@@ -0,0 +1,135 @@
+module.exports = {
+    tags_list: function() {
+      return tags_list;
+    }
+};
+// Bảng tag trong Visual studio code
+var tags_list = { 
+    ////////////CHUYEN 1////////////
+    chuyen_1_a456_9kg: 'DB0,X0.0',
+    chuyen_1_a456_13kg: 'DB0,X0.1',
+    chuyen_1_a456_18kg: 'DB0,X0.2',
+
+    chuyen_1_a789_9kg: 'DB0,X0.3',
+    chuyen_1_a789_13kg: 'DB0,X0.4',
+    chuyen_1_a789_18kg: 'DB0,X0.5',
+
+    chuyen_1_b456_9kg: 'DB0,X0.6',
+    chuyen_1_b456_13kg: 'DB0,X0.7',
+    chuyen_1_b456_18kg: 'DB0,X1.0',
+
+    chuyen_1_b789_9kg: 'DB0,X1.1',
+    chuyen_1_b789_13kg: 'DB0,X1.2',
+    chuyen_1_b789_18kg: 'DB0,X1.3',
+
+    chuyen_1_cl_9kg: 'DB0,X1.4',
+    chuyen_1_cl_13kg: 'DB0,X1.5',
+    chuyen_1_cl_18kg: 'DB0,X1.6',
+
+    ////////////CHUYEN 2////////////
+    chuyen_2_a456_9kg: 'DB0,X2.0',
+    chuyen_2_a456_13kg: 'DB0,X2.1',
+    chuyen_2_a456_18kg: 'DB0,X2.2',
+
+    chuyen_2_a789_9kg: 'DB0,X2.3',
+    chuyen_2_a789_13kg: 'DB0,X2.4',
+    chuyen_2_a789_18kg: 'DB0,X2.5',
+
+    chuyen_2_b456_9kg: 'DB0,X2.6',
+    chuyen_2_b456_13kg: 'DB0,X2.7',
+    chuyen_2_b456_18kg: 'DB0,X3.0',
+
+    chuyen_2_b789_9kg: 'DB0,X3.1',
+    chuyen_2_b789_13kg: 'DB0,X3.2',
+    chuyen_2_b789_18kg: 'DB0,X3.3',
+
+    chuyen_2_cl_9kg: 'DB0,X3.4',
+    chuyen_2_cl_13kg: 'DB0,X3.5',
+    chuyen_2_cl_18kg: 'DB0,X3.6',
+
+    ////////////CHUYEN 3////////////
+    chuyen_3_a456_9kg: 'DB0,X4.0',
+    chuyen_3_a456_13kg: 'DB0,X4.1',
+    chuyen_3_a456_18kg: 'DB0,X4.2',
+
+    chuyen_3_a789_9kg: 'DB0,X4.3',
+    chuyen_3_a789_13kg: 'DB0,X4.4',
+    chuyen_3_a789_18kg: 'DB0,X4.5',
+
+    chuyen_3_b456_9kg: 'DB0,X4.6',
+    chuyen_3_b456_13kg: 'DB0,X4.7',
+    chuyen_3_b456_18kg: 'DB0,X5.0',
+
+    chuyen_3_b789_9kg: 'DB0,X5.1',
+    chuyen_3_b789_13kg: 'DB0,X5.2',
+    chuyen_3_b789_18kg: 'DB0,X5.3',
+
+    chuyen_3_cl_9kg: 'DB0,X5.4',
+    chuyen_3_cl_13kg: 'DB0,X5.5',
+    chuyen_3_cl_18kg: 'DB0,X5.6',
+
+    ////////////CHUYEN 4////////////
+    chuyen_4_a456_9kg: 'DB0,X6.0',
+    chuyen_4_a456_13kg: 'DB0,X6.1',
+    chuyen_4_a456_18kg: 'DB0,X6.2',
+
+    chuyen_4_a789_9kg: 'DB0,X6.3',
+    chuyen_4_a789_13kg: 'DB0,X6.4',
+    chuyen_4_a789_18kg: 'DB0,X6.5',
+
+    chuyen_4_b456_9kg: 'DB0,X6.6',
+    chuyen_4_b456_13kg: 'DB0,X6.7',
+    chuyen_4_b456_18kg: 'DB0,X7.0',
+
+    chuyen_4_b789_9kg: 'DB0,X7.1',
+    chuyen_4_b789_13kg: 'DB0,X7.2',
+    chuyen_4_b789_18kg: 'DB0,X7.3',
+
+    chuyen_4_cl_9kg: 'DB0,X7.4',
+    chuyen_4_cl_13kg: 'DB0,X7.5',
+    chuyen_4_cl_18kg: 'DB0,X7.6',
+
+    ////////////CHUYEN 5////////////
+    chuyen_5_a456_9kg: 'DB0,X8.0',
+    chuyen_5_a456_13kg: 'DB0,X8.1',
+    chuyen_5_a456_18kg: 'DB0,X8.2',
+
+    chuyen_5_a789_9kg: 'DB0,X8.3',
+    chuyen_5_a789_13kg: 'DB0,X8.4',
+    chuyen_5_a789_18kg: 'DB0,X8.5',
+
+    chuyen_5_b456_9kg: 'DB0,X8.6',
+    chuyen_5_b456_13kg: 'DB0,X8.7',
+    chuyen_5_b456_18kg: 'DB0,X9.0',
+
+    chuyen_5_b789_9kg: 'DB0,X9.1',
+    chuyen_5_b789_13kg: 'DB0,X9.2',
+    chuyen_5_b789_18kg: 'DB0,X9.3',
+
+    chuyen_5_cl_9kg: 'DB0,X9.4',
+    chuyen_5_cl_13kg: 'DB0,X9.5',
+    chuyen_5_cl_18kg: 'DB0,X9.6',
+
+    ////////////CHUYEN 6////////////
+    chuyen_6_a456_9kg: 'DB0,X10.0',
+    chuyen_6_a456_13kg: 'DB0,X10.1',
+    chuyen_6_a456_18kg: 'DB0,X10.2',
+
+    chuyen_6_a789_9kg: 'DB0,X10.3',
+    chuyen_6_a789_13kg: 'DB0,X10.4',
+    chuyen_6_a789_18kg: 'DB0,X10.5',
+
+    chuyen_6_b456_9kg: 'DB0,X10.6',
+    chuyen_6_b456_13kg: 'DB0,X10.7',
+    chuyen_6_b456_18kg: 'DB0,X11.0',
+
+    chuyen_6_b789_9kg: 'DB0,X11.1',
+    chuyen_6_b789_13kg: 'DB0,X11.2',
+    chuyen_6_b789_18kg: 'DB0,X11.3',
+
+    chuyen_6_cl_9kg: 'DB0,X11.4',
+    chuyen_6_cl_13kg: 'DB0,X11.5',
+    chuyen_6_cl_18kg: 'DB0,X11.6'
+    
+    
+};
